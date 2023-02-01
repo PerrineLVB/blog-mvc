1cb0ff4c29cf39140e63eef82f08fa58ff50d644
PerrineLVB <perrine.launay@wanadoo.fr>
2023-02-01 16:21:01 +0100

    dernière version avec commentaires en ajax

--- readPost.php
+++ readPost.php
@@ -14,8 +14,8 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
     //on va chercher les informations de l'article qu'on souhaite afficher
     $post = PostManager::getPostById($id); //les infos de l'article
-    $post_categories = CategoryManager::getCategoriesByPostId($id);
-    $post_user = UserManager::getUserInfosByPostId($id); //les categories auxquelles il est relié
+    $post_categories = CategoryManager::getCategoriesByPostId($id); //les categories auxquelles il est relié
+    $post_user = UserManager::getUserInfosByPostId($id);
     $comments = CommentManager::getCommentsByPostId($id);
     $commentsData = [];
     foreach ($comments as $comment) {
@@ -25,16 +25,12 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
             'author' => $comment_author,
         ];
     }
-    if (isset($_SESSION['user'])) {
-        if (isset($_POST) && !empty($_POST)) {
-            $content = $_POST['content'];
-            $id_user = $_SESSION['user']['id'];
-            $id_post = $post->getIdPost();
-            $newCommentId = CommentManager::addComment($content, $id_user, $id_post);
-            header("location:readPost.php?id=$id_post#comments");
-        }
-    } else {
-        echo "Vous devez être connecté(e) pour pouvoir ajouter un commentaire.";
+    if (isset($_POST) && !empty($_POST)) {
+        $content = $_POST['content'];
+        $id_user = $_SESSION['user']['id'];
+        $id_post = $post->getIdPost();
+        CommentManager::addComment($content, $id_user, $id_post);
+        // header("location:readPost.php?id=$id_post#comments");
     }
 }
 
--- views/readPostView.php
+++ views/readPostView.php
@@ -18,36 +18,41 @@ require_once 'partials/header.php';
             <a href="deletePost.php?id=<?php echo $post->getIdPost() ?>"><button type="button" class="btn btn-outline-danger h-75">Supprimer l'article <i class="fa-regular fa-trash-can"></i></button></a>
         </div>
     </div>
-    <div class="container" id="comments">
+    <div class="container">
         <div class="card border border-dark-subtle rounded mt-5">
             <div class="card-header">
                 <h4 class="mt-2">Commentaires</h4>
             </div>
             <div class="card-body">
-                <?php if (isset($commentsData) && !empty($commentsData)) {
-                    foreach ($commentsData as $commentData) { ?>
-                        <figure class="ps-5">
+                <div id="comments">
+                    <?php if (isset($commentsData) && !empty($commentsData)) {
+                        foreach ($commentsData as $commentData) { ?>
+                            <figure class="ps-5">
+                                <blockquote class="blockquote">
+                                    <p><?php echo $commentData['comment']->getContent() ?></p>
+                                </blockquote>
+                                <figcaption class="blockquote-footer">
+                                    <em><?php echo $commentData['author']->getPseudo() ?></em>
+                                    <br>
+                                    <em><?php echo $commentData['comment']->getDate() ?></em>
+                                </figcaption>
+                            </figure>
+                        <?php }
+                    } else { ?>
+                        <figure class="text-center">
                             <blockquote class="blockquote">
-                                <p><?php echo $commentData['comment']->getContent() ?></p>
+                                <p>Aucun commentaire</p>
                             </blockquote>
-                            <figcaption class="blockquote-footer">
-                                <em><?php echo $commentData['author']->getPseudo() ?>
-                                </em><br>
-                                <em><?php echo $commentData['comment']->getDate() ?></em>
-                            </figcaption>
                         </figure>
-                    <?php }
-                } else { ?>
-                    <figure class="text-center">
-                        <blockquote class="blockquote">
-                            <p>Aucun commentaire</p>
-                        </blockquote>
-                    </figure>
+                    <?php } ?>
+                </div>
+                <?php if (isset($_SESSION['user'])) { ?>
+                    <form method="post" class="mt-5">
+                        <textarea class="d-block m-auto w-50 rounded p-2" rows="5" placeholder="Votre commentaire..." name="content" id="inputContent"></textarea>
+                        <input id="pseudo" type="hidden" value="<?php echo $_SESSION['user']['pseudo'] ?>">
+                        <button type="submit" class="btn btn-outline-danger d-block m-auto mt-3">Ajouter un commentaire</button>
+                    </form>
                 <?php } ?>
-                <form method="post" class="mt-5">
-                    <textarea class="d-block m-auto w-50 rounded p-2" rows="5" placeholder="Votre commentaire..." name="content" urr></textarea>
-                    <button type="submit" class="btn btn-outline-danger d-block m-auto mt-3">Ajouter un commentaire</button>
-                </form>
             </div>
         </div>
     </div>
