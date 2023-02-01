199f704021723a05d72aca6b5c3cfecf56b84a9e
PerrineLVB <perrine.launay@wanadoo.fr>
2023-02-01 16:43:50 +0100

    dernière version avec commentaires en ajax

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         let userPseudo = $('#pseudo').val();
         let date = 'a few moments ago';
         $.post('', { content: comment });                                               // on l'envoie en base de données via la requête Ajax
-        $('<figure class="ps-5"><blockquote class= "blockquote" ><p>' + comment + '</p></blockquote ><figcaption class="blockquote-footer"><em>' + userPseudo + '</em><br><em>' + date + '</em></figcaption></figure >').appendTo('#comments');    // affichage immédiat du commentaire en ajoutant un élément dans le DOM
+        $('<figure class="ps-5"><blockquote class= "blockquote" ><p>' + comment + '</p></blockquote ><figcaption class="blockquote-footer"><em>' + userPseudo + '</em><br><em>' + date + '</em></figcaption></figure >').appendTo('#comments');                                  // affichage immédiat du commentaire en ajoutant un élément dans le DOM
         $('#inputContent').val('');                                                     // on vide le champ de saisie du commentaire pour simuler l'envoi
     })
 });
